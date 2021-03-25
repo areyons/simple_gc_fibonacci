f90543f98ad45842219b41a44e20b56c663d2c32
Andrew <andreiono@ukr.net>
2021-03-25 13:15:41 +0200

    Complete Function

---
+++ index.js
@@ -0,0 +1,46 @@
+exports.fibonacci = (req, res) => {
+
+    let compute;
+
+    // make array with Fibonacci numbers which is limited by a num  
+    function fibonacci(num) {
+        let fib = [0, 1];
+        for (let i = 2; i < parseInt(num); i++) {
+            fib[i] = fib[i - 2] + fib[i - 1];
+        }
+        return fib;
+    }
+
+    // get array and calc sum of odd numbers
+    function isOddSumm(array) {
+        let res = 0;
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 1) {
+                res =+ array[i];
+            }
+        }
+        return res;
+    }
+
+    // check that request is in json format
+    if (req.get('content-type') == 'application/json') {
+
+        // get compute value from json
+        ({compute} = req.body);
+
+        // check if that value is a number
+        if (Number.isInteger(compute)) {
+
+            // return result in json format
+            res.status(200).send(JSON.stringify({
+                result: isOddSumm(fibonacci(compute))
+            }));
+
+        } else {
+            res.status(404).send('I need only a number! But you put a ' + typeof(compute));
+        }
+    } else {
+        res.status(404).send('I need only json format!');
+    }
+
+};
